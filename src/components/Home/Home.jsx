3b64d390315d58a6abee04ae3f2e059392bf0be5
KishoreXLR8 <kishoresworks8@gmail.com>
2023-05-08 21:14:35 +0530

    Initial commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,12 @@
 import{Link} from 'react-router-dom';
-import LogoTitle from'../../assets/images/logo-s.png';
 import'./Home.scss';
-import Logo from './Logo/Logo';
 import { useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters/AniLetters';
 import Loader from 'react-loaders';
 
 const Home=() =>{
     const[letterClass] = useState('text-animate')
-    const nameArray = ["h","y","a","m"]
+    const nameArray = ["","S","h","y","a","m"]
     const jobArray =['w','e','b',' ','d','e','v','e','l','o','p','e','r']
 
     
@@ -24,7 +22,7 @@ const Home=() =>{
                     <br/> 
                     <span className={`${letterClass}_13`}>I</span>
                     <span className={`${letterClass}_14`}>'m</span>
-                    <img src={LogoTitle} alt="developer" />
+                    
                     <AnimatedLetters letterClass={letterClass}
                     strArray={nameArray}
                     idx={15}/>
@@ -36,7 +34,7 @@ const Home=() =>{
                 <h2>Frontend developer/Freelancer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
-            <Logo/>
+            
         </div>
         <Loader type='pacman'/>;
         </>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,6 @@
-import { Link, NavLink } from 'react-router-dom'
+import {  NavLink } from 'react-router-dom'
 import './Sidebar.scss'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faHome,faUser,faEnvelope} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
@@ -9,10 +8,9 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () =>(
     <div className = 'nav-bar'>
-        <Link className='logo'>
-            <img src = {LogoS} alt='logo'/>
-            <img className="sub-logo" src = {LogoSubtitle} alt='Shyam'/>
-        </Link>
+        
+           
+            
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">  
             <FontAwesomeIcon icon ={faHome} color="#4d4d4e"/>
